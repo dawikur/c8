52eb011bad4549f17b947d218f803c24aa49f68b
Dawid Kurek <dawikur@gmail.com>
2016-05-06 16:59:36 +0200

    Proper build with nana

--- src/c8.cpp
+++ src/c8.cpp
@@ -1,5 +1,16 @@
 // Copyright 2016 Dawid Kurek. All Rights Reserved.
 
+
+#include <stdexcept>
+#include <memory>
+#include <vector>
+
+#include <nana/gui/wvl.hpp>
+
 int main(void) {
+  nana::form form;
+  form.show();
+  nana::exec();
+
   return 0;
 }
